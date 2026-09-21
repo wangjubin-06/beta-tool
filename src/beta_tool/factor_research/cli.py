3ba570325f97b01b3e3555f2efbae9829f1e12da
Wang Jubin <79356822+wangjubin-06@users.noreply.github.com>
2026-09-21 16:16:44 +0800

    allow hac for factors cli

--- src/beta_tool/factor_research/cli.py
+++ src/beta_tool/factor_research/cli.py
@@ -11,6 +11,7 @@ def factors(
     end_date:str | None = typer.Option(None, "--end_date", "-e", help="End date of factor decomposition window (optional)"),
     return_type:str = typer.Option('simple', "--return_type", "-r", help="Calculate returns in 'simple' or 'log' (optional, defaults to 'simple')"),
     frequency:str = typer.Option('daily',"--frequency", "-f", help="Frequency of data. Available in 'daily', 'weekly', 'monthly' (optional, defaults to 'daily')"),
+    hac: bool = typer.Option(True, "--hac", help="Use HAC-aware statistics (optional, defaults to True)"),
     advanced: bool = typer.Option(False, "--advanced", "-a", help="Choose to show advanced regression results (optional)")
 ):
     """Analyses the factor loadings of assets.
@@ -20,13 +21,23 @@ def factors(
 
     with console.status("[bold green]Fetching price data and running regression..."):
 
-        obj = EquityFactorsRegression(
-            factor_source=factor_source,
-            start_date=start_date,
-            end_date=end_date,
-            return_type=return_type,
-            frequency=frequency,
-        )
+        if hac:
+            obj = EquityFactorsRegression(
+                factor_source=factor_source,
+                start_date=start_date,
+                end_date=end_date,
+                return_type=return_type,
+                frequency=frequency,
+                hac='auto'
+            )
+        else:
+            obj = EquityFactorsRegression(
+                factor_source=factor_source,
+                start_date=start_date,
+                end_date=end_date,
+                return_type=return_type,
+                frequency=frequency,
+            )
 
         obj.asset_list(*tickers)
         obj.regress()
